8c01e6a690a259bbbb501c2b0b4f7462734b6546
Pbris <peter.landbris@gmail.com>
2024-07-27 15:36:38 +0200

    Added new table book for assignment, and route for CREATE with form

---
+++ src/Controller/BookController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Book;
+use Doctrine\Persistence\ManagerRegistry;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+    #[Route('/book/create', name: 'book_create', methods: ['GET'])]
+    public function createBookForm(): Response
+    {
+        return $this->render('book/create.html.twig');
+    }
+
+    #[Route('/book/create', name: 'book_create_post', methods: ['POST'])]
+    public function createBook(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $book = new Book();
+        $book->setTitle($request->request->get('title'));
+        $book->setIsbn($request->request->get('isbn'));
+        $book->setAuthor($request->request->get('author'));
+        $book->setImage($request->request->get('image_filename'));
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return new Response('Saved new book with id ' . $book->getId());
+    }
+}
