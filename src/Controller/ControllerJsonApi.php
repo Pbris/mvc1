b0bd79cc560830f0b58992a527bc9f3652dc3355
Pbris <peter.landbris@gmail.com>
2024-07-29 17:32:38 +0200

    Added JSON routes for showing all books, and book by ISBN, in kmom05 assignment.

--- src/Controller/ControllerJsonApi.php
+++ src/Controller/ControllerJsonApi.php
@@ -7,11 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Exception;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use App\Card\Deck;
 use App\Game\BlackjackGame;
+use App\Entity\Book;
 
-class ControllerJsonApi
+class ControllerJsonApi extends AbstractController
 {
     #[Route("/api/deck", methods: ['GET'])]
     public function getDeck(): Response
@@ -118,5 +121,29 @@ class ControllerJsonApi
         ];
 
         return new JsonResponse($data);
+    }
+
+    #[Route('/api/library/books', name: 'api_library_books', methods: ['GET'])]
+    public function getAllBooks(ManagerRegistry $doctrine): JsonResponse
+    {
+        $books = $doctrine->getRepository(Book::class)->findAll();
+
+        $response = $this->json($books);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE 
+        );
+        return $response;
+    }
+
+    #[Route('/api/library/book/{isbn}', name: 'api_library_book', methods: ['GET'])]
+    public function getBookByIsbn(string $isbn, ManagerRegistry $doctrine): JsonResponse
+    {
+        $book = $doctrine->getRepository(Book::class)->findOneBy(['isbn' => $isbn]);
+
+        $response = $this->json($book);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE 
+        );
+        return $response;
     }
 }
